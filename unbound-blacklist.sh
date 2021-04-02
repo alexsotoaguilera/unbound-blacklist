be0739ace6aa71fc52302d0d6af6dd4f07e5e3fd
Alex Soto Aguilera <alex.soto.aguilera@gmail.com>
2021-04-02 14:28:59 +0200

    blacklist url uses var

--- unbound-blacklist.sh
+++ unbound-blacklist.sh
@@ -4,6 +4,6 @@ BLACKLIST_URL=https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 BLACKLIST_PATH=/etc/unbound/unbound-blacklist.orig
 UNBOUND_CONF=/etc/unbound/unbound.conf.d/unbound-blacklist.conf
 
-curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o $BLACKLIST_PATH
+curl $BLACKLIST_URL -o $BLACKLIST_PATH
 echo "server:" > $UNBOUND_CONF
 cat $BLACKLIST_PATH | grep '^0\.0\.0\.0' | awk '{print "    local-zone: \""$2"\" redirect\n    local-data: \""$2" A 0.0.0.0\""}' >> $UNBOUND_CONF
